b8c42f437f2aba7ce512f8d1374ad3d17ffa81e8
Camden <me@12oclock.dev>
2021-12-10 00:35:31 -0600

    Relocate dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,13 +36,24 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-toml:2.13.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("com.github.seeseemelk:MockBukkit-v1.17:1.10.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("com.github.seeseemelk:MockBukkit-v1.17:1.10.4")
 }
 
 
 tasks {
+
     test {
         useJUnitPlatform()
     }
+
+    shadowJar {
+        relocate("com.fasterxml", "dev.twelveoclock.plugintemplate.libs.com.fasterxml")
+        relocate("org.jetbrains", "dev.twelveoclock.plugintemplate.libs.org.jetbrains")
+        relocate("org.intellij", "dev.twelveoclock.plugintemplate.libs.org.intellij")
+    }
+}
+
+tasks {
+
 }
